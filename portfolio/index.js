56071f068a361340a420b2573e0b53f55e8714c2
Louise-Birulia-Bezenson <elizabethbirulia@gmail.com>
2022-01-31 23:31:14 +0300

    feat: add translate and chenge theme

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,11 +1,15 @@
+import i18Obj from './translate.js';
+
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.main-nav');
 const navLinks = document.querySelectorAll('.nav-link');
 //Part3
 const portfolioBtn = document.querySelectorAll('.portfolio-box-button');
 const portfolioImages = document.querySelectorAll('.portfolio-photo-item');
+const pageLanguages = document.querySelectorAll("[data-lang]");
+const valueSwitch = document.querySelectorAll('.switch');
 
-// const portfolioActivButton = document.querySelectorAll('.active');
+
 
 function toggleMenu() {
   hamburger.classList.toggle('is-active');
@@ -41,8 +45,60 @@ function preloadImages(season) {
 const seasons = ['winter', 'spring', 'summer', 'autumn'];
 seasons.forEach(season => preloadImages(season));
 
-console.log(`
-Ваша отметка - 85 балла(ов)
-Отзыв по пунктам ТЗ:
+function getTranslate(lang){
+  const elements = document.querySelectorAll("[data-i18n]");
+  elements.forEach((element) => {
+    const str = i18Obj[lang][element.dataset.i18n];
+    if (element.placeholder) {
+      element.placeholder = str;
+    }
+    else{element.textContent = str;
+    }
 
-Все пункты выполнены полностью!`);
+  });
+}
+
+getTranslate(localStorage.getItem('lang'));
+
+pageLanguages.forEach((el) => {
+  el.addEventListener('click', (clickedEl)=> { 
+    getTranslate(el.dataset.lang);
+    pageLanguages.forEach(item => item.classList.remove("lang-active"));
+    clickedEl.target.classList.add("lang-active");
+    localStorage.setItem('lang', clickedEl.target.dataset.lang);
+
+  });
+});
+
+valueSwitch.forEach((el) => el.addEventListener('click', changeLighting));
+
+
+function changeLighting(el){
+  valueSwitch.forEach((el)=> el.classList.remove('switch-activ'));
+  el.target.classList.add("switch-activ");
+  document.body.classList.toggle("light");
+
+//   document.documentElement.style.setProperty('--body-color', '#fff');
+// document.documentElement.style.setProperty('--text-color', '#000');
+// document.documentElement.style.setProperty('--hover-color', '#000');
+// document.documentElement.style.setProperty('--social-filter', 'invert(100%)');
+// document.documentElement.style.setProperty('--title-primary-color','#000');
+
+}
+
+
+
+console.log(`
+Ваша отметка - 78 балла(ов)
+Отзыв по пунктам ТЗ:
+Не выполненные:
+1) сложные эффекты для кнопок при наведении и/или клике
+
+Частично выполненные пункты:
+1) выбранный пользователем язык отображения страницы и светлая или тёмная тема сохраняются при перезагрузке страницы — 3 балл(а)
+Комментарий проверяющего: Реализовано только для смены языка.
+
+
+Все оставшиеся пункты выполнены и не имеют комментариев проверяющего.
+`);
+

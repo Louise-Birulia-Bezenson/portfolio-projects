7e3136fd6f41df9e65ec9afad0f14c3d9057624e
Louise-Birulia-Bezenson <elizabethbirulia@gmail.com>
2022-04-25 01:35:47 +0300

    feat: add popup

--- shelter/assets/js/script.js
+++ shelter/assets/js/script.js
@@ -1,6 +1,9 @@
 let navItems = document.querySelectorAll(".nav-items");
 let checkBox = document.getElementById('menu-toggle');
 let overlay = document.querySelectorAll(".overlay");
+let btnCloseAboutPet = document.getElementsByClassName('close-about-pet')[0];
+
+let learnMore = document.querySelectorAll(".slider-button");
 
 if (document.getElementById("menu-toggle").checked === true){
     document.body.style.overflow = "hidden";  
@@ -16,6 +19,19 @@ function closesNav() {
     document.body.style.overflow = "auto";
 }
 
+function openAboutPet(){
+    document.getElementById('frame-about-pet').style.display = 'block';
+    document.getElementById('about-pet').style.display = 'block';
+    btnCloseAboutPet.style.display = 'block';
+    document.body.style.overflow = "hidden";
+}
+function closeAboutPet(){
+    document.getElementById('frame-about-pet').style.display = 'none';  
+    document.getElementById('about-pet').style.display = 'none';
+    btnCloseAboutPet.style.display = 'none';
+    document.body.style.overflow = "auto";
+}
+
 navItems.forEach(element => {
     element.addEventListener('click',closesNav);
 });
@@ -23,3 +39,12 @@ navItems.forEach(element => {
 overlay.forEach(el => {
    el.addEventListener('click',closesNav);
 });
+
+learnMore.forEach(element=>{
+    element.addEventListener('click',openAboutPet);
+});
+document.getElementById('frame-about-pet').addEventListener('click', closeAboutPet);
+
+btnCloseAboutPet.addEventListener('click', closeAboutPet);
+
+
